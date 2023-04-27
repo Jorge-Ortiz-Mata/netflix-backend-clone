2ada2d8fba2f4710883c821bc427f246a1b2152a
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-04-26 20:25:04 -0500

    Adding sessions create method and render the response.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class SessionsController < ApplicationController
       before_action :return_email, only: %i[validate_email]
+      before_action :set_user, only: %i[create]
 
       def validate_email
         user = User.find_by(email: @email)
@@ -13,10 +14,31 @@ module Api
         end
       end
 
+      def create
+        if @user&.authenticate(user_password)
+          @user.access_token.delete if @user.access_token.present?
+
+          payload = { user: @user.email }
+          token = JWT.encode payload, ENV['APP_JWT_KEY'], 'HS256'
+          @user.create_access_token(token: token)
+          render json: { access_token: token }, status: :ok
+        else
+          render json: { errors: ['Credentials are incorrect'] }, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def return_email
         @email = JSON.parse(params[:email])
+      end
+
+      def set_user
+        @user = User.find_by(email: JSON.parse(params[:user])['email'])
+      end
+
+      def user_password
+        JSON.parse(params[:user])['password']
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,14 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      # Access Tokens routes.
       delete 'access_token/delete', to: 'access_tokens#destroy'
+
+      # User routes
       resources :users, only: %i[create update destroy]
+
+      # Sessions routes
+      post "sessions/create", to: "sessions#create"
       get "sessions/validate/email", to: "sessions#validate_email"
     end
   end
